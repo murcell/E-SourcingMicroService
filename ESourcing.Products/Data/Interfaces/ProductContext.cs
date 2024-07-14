74c6aed7b17b77d723a6e4cd3673268ff999c81e
murcell <murselgurkan@gmail.com>
2024-07-14 18:30:14 +0300

    Product microservices was coded

---
+++ ESourcing.Products/Data/Interfaces/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using ESourcing.Products.Entities;
+using ESourcing.Products.Settings;
+using MongoDB.Driver;
+
+namespace ESourcing.Products.Data.Interfaces
+{
+    public class ProductContext : IProductContext
+    { 
+        public ProductContext(IProductDatabaseSettings productDatabaseSettings)
+        {
+            var client = new MongoClient(productDatabaseSettings.ConnectionStrings);
+            var database = client.GetDatabase(productDatabaseSettings.DatabaseName);
+            Products = database.GetCollection<Product>(productDatabaseSettings.ProductCollectionName);
+
+            ProductContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
--- ESourcing.Products/Program.cs
+++ ESourcing.Products/Program.cs
@@ -1,49 +1,26 @@
-using Microsoft.AspNetCore.Cors.Infrastructure;
+using ESourcing.Products.Data.Interfaces;
+using ESourcing.Products.Repositories;
+using ESourcing.Products.Repositories.Interfaces;
+using ESourcing.Products.Settings;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-//{
-//    // bu microservise tokendaðýtmaktan görevli arkadaþ
-//    options.Authority = builder.Configuration["IdentityServerUrl"];
-//    options.Audience = "resource_catalog";
-//    options.RequireHttpsMetadata = false;
-//});
+builder.Services.AddControllers(opt =>
+{
+    // tüm kontrolleri authorize ettik
+    //opt.Filters.Add(new AuthorizeFilter());
+});
 
-//builder.Services.AddControllers(opt =>
-//{
-//    // tüm kontrolleri authorize ettik
-//    opt.Filters.Add(new AuthorizeFilter());
-//});
+builder.Services.Configure<ProductDatabaseSettings>(builder.Configuration.GetSection("ProductDatabaseSettings"));
 
-//builder.Services.AddScoped<ICategoryService, CategoryService>();
-//builder.Services.AddScoped<ICourseService, CourseService>();
-//builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddSingleton<IProductDatabaseSettings>(sp =>  sp.GetRequiredService<IOptions<ProductDatabaseSettings>>().Value );
 
-//builder.Services.AddSingleton<IDatabaseSetttings>(sp =>
-//{
-//    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
-//});
+builder.Services.AddTransient<IProductContext, ProductContext>();
+builder.Services.AddTransient< IProductRepository, ProductRepository>();
 
-//builder.Services.AddMassTransit(x =>
-//{
-//    x.UsingRabbitMq((context, cfg) =>
-//    {
-//        cfg.Host(builder.Configuration["RabbitMQUrl"], "/", host =>
-//        {
-//            host.Username("guest");
-//            host.Password("guest");
-//        });
 
-//    });
-//});
-
-//builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
