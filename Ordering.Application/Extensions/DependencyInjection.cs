378a01acf5b2d30ffda4c256ed98f2123cd17065
murcell <murselgurkan@gmail.com>
2024-07-19 16:07:19 +0300

    Related pipeline behaviors were developed

--- Ordering.Application/Extensions/DependencyInjection.cs
+++ Ordering.Application/Extensions/DependencyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Commands.OrderCreate;
 using Ordering.Application.Handlers;
 using Ordering.Application.Mapper;
+using Ordering.Application.PipelineBehaviors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,19 +18,20 @@ namespace Ordering.Application.DependencyInjections
 {
     public static class DependencyInjection
     {
+       
         public static void AddApplication(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            services.AddFluentValidationAutoValidation(config =>
-            {
-               // config.DisableDataAnnotationsValidation = true;
-            });
-
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(typeof(OrderCreateCommandHandler).Assembly);
                
             });
+
+            services.AddTransient(typeof(IPipelineBehavior<,>),typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
 
             var config = new MapperConfiguration(cfg =>
             {
