4b6ee161acb30e8fa3a744c2ed80cc652887d623
murcell <murselgurkan@gmail.com>
2024-07-19 16:51:25 +0300

    EventBusOrderCreateConsumer was coded

---
+++ ESourcing.Order/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using ESourcing.Order.Consumers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ESourcing.Order.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static EventBusOrderCreateConsumer Listener { get; set; }
+
+        public static IApplicationBuilder UseEventBusListener(this IApplicationBuilder app) 
+        {
+            Listener = app.ApplicationServices.GetService<EventBusOrderCreateConsumer>();
+
+            var lifeTimeScope = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+            lifeTimeScope.ApplicationStarted.Register(OnStarted);
+            lifeTimeScope.ApplicationStarted.Register(OnStopping);
+
+            return app;
+        }
+
+        private static void OnStarted()
+        {
+            Listener.Consume();
+        }
+
+        private static void OnStopping()
+        {
+            Listener.Disconnect();
+        }
+
+    }
+}
--- ESourcing.Order/Program.cs
+++ ESourcing.Order/Program.cs
@@ -1,9 +1,13 @@
 using ESourcing.Order.Extensions;
+using EventBusRabbitMQ.Producer;
+using EventBusRabbitMQ;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Ordering.Application.DependencyInjections;
 using Ordering.Infastructure;
 using Ordering.Infastructure.Extensions;
+using RabbitMQ.Client;
+using ESourcing.Order.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +21,41 @@ builder.Services.AddControllers(opt =>
 builder.Services.AddInfastructure(builder.Configuration);
 builder.Services.AddApplication();
 
+#region EventBus
+
+builder.Services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+{
+    var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+    var factory = new ConnectionFactory()
+    {
+        HostName = builder.Configuration["EventBus:HostName"],
+    };
+
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:UserName"]))
+    {
+        factory.UserName = builder.Configuration["EventBus:UserName"];
+    }
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:Password"]))
+    {
+        factory.Password = builder.Configuration["EventBus:Password"];
+    }
+
+    var retryCount = 5;
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:RetryCount"]))
+    {
+        retryCount = int.Parse(builder.Configuration["EventBus:RetryCount"]);
+    }
+
+    return new DefaultRabbitMQPersistentConnection(factory, retryCount, logger);
+
+});
+
+
+builder.Services.AddSingleton<EventBusOrderCreateConsumer>();
+
+ #endregion
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +64,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.MigrateAndSeedDatabase();
+
+app.UseEventBusListener();
 
 
 // Configure the HTTP request pipeline.
